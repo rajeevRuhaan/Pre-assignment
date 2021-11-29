eca6a5b60310a3e663eb3cc010d16f4e796d55e5
Rajeev <s2100155@edu.bc.fi>
2021-11-29 10:04:02 +0200

    userinfo implement filter

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,34 +5,40 @@ import Button from "react-bootstrap/Button";
 
 const UserInfo = ({ users }) => {
   const { id } = useParams(); //useParams used to remember the parameter we passed using links.
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // useNavigate uses in react-router-v6
 
   return (
     <div className="user">
       <Card className="userinfo">
-        <p>Name: {users[id - 1].name}</p>
-        <p>User Name: {users[id - 1].username}</p>
-        <p>E.mail: {users[id - 1].email}</p>
-        <p>Phone: {users[id - 1].phone}</p>
-        <p>Company: {users[id - 1].company.name}</p>
-        <p>
-          Website:
-          <a
-            href={`https://${users[id - 1].website}`}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {" "}
-            https://{users[id - 1].website}
-          </a>
-        </p>
-        <p>
-          <b>Address:</b>
-        </p>
-        <li>street: {users[id - 1].address.street}</li>
-        <li>suite: {users[id - 1].address.suite}</li>
-        <li>City: {users[id - 1].address.city}</li>
-        <li>Zipcode: {users[id - 1].address.zipcode}</li>
+        {users
+          .filter((user) => user.id === parseInt(id))
+          .map((user) => (
+            <>
+              <p>Name: {user.name}</p>
+              <p>User Name: {user.username}</p>
+              <p>E.mail: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+              <p>Company: {user.company.name}</p>
+              <p>
+                Website:
+                <a
+                  href={`https://${user.website}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {" "}
+                  https://{user.website}
+                </a>
+              </p>
+              <p>
+                <b>Address:</b>
+              </p>
+              <li>street: {user.address.street}</li>
+              <li>suite: {user.address.suite}</li>
+              <li>City: {user.address.city}</li>
+              <li>Zipcode: {user.address.zipcode}</li>
+            </>
+          ))}
       </Card>
       <>
         <Button
